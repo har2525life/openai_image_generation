a96080b104a5a04faf819fc0e9d1d71fd9516cf7
har2525life <har2525life@outlook.com>
2023-06-12 10:12:21 +0900

    add: error page

---
+++ src/app/error.tsx
@@ -0,0 +1,14 @@
+import { deflate } from "zlib"
+
+const Error = () => {
+    return (
+        <div>
+            <div className="text-center text-5xl font-bold mb-3 text-white">500</div>
+            <div className="text-center text-xl font-bold text-white">Server Error</div>
+        </div>
+    )
+}
+
+export default Error
+
+// サーバコンポーネントでエラーが起きたときに表示される画面
