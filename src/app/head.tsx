0bdb41cf0f43a6c2638a2d3cb54391efda22f83c
har2525life <har2525life@outlook.com>
2023-06-12 10:04:14 +0900

    add: head data

---
+++ src/app/head.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <>
+    <title>Image Generation</title>
+    <meta content="width=device-width, initial-scale=1" name='viewport' />
+    <meta name="description" content='Image Generation' />
+    <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
+
+export default Head
