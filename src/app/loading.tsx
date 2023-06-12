f8a271c61396adf61c7ee4d3c24d2bc1b99599fa
har2525life <har2525life@outlook.com>
2023-06-12 10:12:28 +0900

    add: loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+    return (
+        <div className="flex justify-center">
+            <div className="h-10 w-10 animation-spin rounded-4 border-yellow-500 border-t-transparent"></div>
+        </div>
+    )
+}
+
+export default Loading
+
+//データ取得中に自動的に表示される画面
