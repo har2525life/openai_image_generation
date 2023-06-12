3cb0ba34819271d3da9b9c380898829d6d36830e
har2525life <har2525life@outlook.com>
2023-06-12 10:12:41 +0900

    add: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+const NotFound = () => {
+    return (
+        <div>
+            <div className="text-center text-5xl font-bold mb-3 text-white">404</div>
+            <div className="text-center text-xl font-bold text-white">Not Found</div>
+        </div>
+    )
+}
